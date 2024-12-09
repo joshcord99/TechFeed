447e867e2afa546e69a93a0d52535cc1b97d8aa2
Joshua Cordial <joshuacordial@Joshuas-MacBook-Pro.local>
2024-12-08 17:47:40 -0800

    finished templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
-from app.routes import home
 from app.routes import home, dashboard
+from app.db import init_db
+from flask import g
+from app.utils import filters
+
 def create_app(test_config=None):
     # set up app config
     app = Flask(__name__, static_url_path='/')
@@ -8,6 +11,10 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     )
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
+
 
     # Example route within the factory
     @app.route('/hello')
@@ -17,4 +24,14 @@ def create_app(test_config=None):
     # register routes (blueprints)
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    init_db(app)
+
     return app
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
